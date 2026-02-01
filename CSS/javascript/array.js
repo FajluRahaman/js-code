c638e5e5cd3911ddd82b9dc5eb06a83b2c5bd0e2
fajlu <fajlurahaman185@gmail.com>
2026-02-01 20:15:01 +0530

    messages

--- CSS/javascript/array.js
+++ CSS/javascript/array.js
@@ -4,5 +4,32 @@ const myHeroes = ["shaktiman,naag"];
 
 
 const myArr = [1,2,3,4];
-console.log(myArr[2]);
+// console.log(myArr[2]);
+
+// //Array Methods
+// myArr.push(6)
+// myArr.pop()
+//console.log(myArr);
+//myArr.shift()
+//console.log(myArr.includes(9));
+//console.log(myArr.indexOf(3));
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log( newArr);
+
+//slice,splice
+console.log("A",myArr);
+
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1);
+console.log("B",myArr);
+
+
+
+
+
+
+
 
