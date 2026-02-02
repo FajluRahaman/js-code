3db4af465fbf6bdc70dc3ec14baccc6908489d41
fajlu <fajlurahaman185@gmail.com>
2026-02-02 19:54:34 +0530

    update

---
+++ vsix.js
@@ -0,0 +1,48 @@
+let score =  true
+
+    //console.log( typeof score ); 
+
+//console.log( typeof (score) ); 
+
+//let valueInNumber = Number(score)
+
+//console.log( typeof valueInNumber ); 
+
+//console.log( valueInNumber ); 
+
+let isLoggedIn = "fajlu"
+ 
+let booleanIsLoggedIn =Boolean(isLoggedIn )
+//console.log( booleanIsLoggedIn); 
+
+let someNumber = 56
+
+let stringNumber = String(someNumber)
+//console.log( stringNumber );
+//console.log(  typeof stringNumber );
+
+// operations
+
+console.log(2**3);
+
+let str1 = "string"
+let str2 = "stringkjhb"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log(1 + "2" );
+console.log("1" + 2 + 2 );
+console.log( 2 + 2 + "1" );
+
+console.log( (1 + 4) * 5 / 4);
+
+console.log( +true );
+ 
+let num1, num2, num3
+num1 =num2 =num3 = 2+2
+
+let gameCounter = 100
+
+++gameCounter;
+
+console.log( gameCounter);
