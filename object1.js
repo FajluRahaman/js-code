18dc7a2441009f43666d3967ac697d1f61bcf56f
fajlu <fajlurahaman185@gmail.com>
2026-02-02 19:36:12 +0530

    update

--- object1.js
+++ object1.js
@@ -39,11 +39,21 @@ const users = [
         email: "h@gmail.com"
     },
 ]
-users[1].email
-console.log(tinderUser);
+// users[1].email
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
-console.log(tinderUser.hasOwnProperty('name'));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+// console.log(tinderUser.hasOwnProperty('name'));
+const course = {
+    coursename: "js in hindi",
+    price: "999"
+}
+
+const {coursename:name} = course
+// console.log(coursename);
+console.log(name);
+
+
 
